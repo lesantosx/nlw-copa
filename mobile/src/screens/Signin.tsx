20bc4edb08833e84f0a08a1b82d56cecc3bc8845
Letícia Santos <leticia.santos1997@outlook.com>
2022-11-01 21:59:04 -0300

    NLW Copa projects

---
+++ mobile/src/screens/Signin.tsx
@@ -0,0 +1,12 @@
+import { Text, Center } from 'native-base';
+
+export function Signin(){
+  return (
+    <Center flex={1} bgColor="gray.900">              
+      <Text color="white" fontFamily="heading">
+        Signin
+      </Text>
+    </Center>
+  )
+
+}
